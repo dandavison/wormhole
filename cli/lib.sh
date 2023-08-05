d8f0182a2fefeea7d3b89e9514c98d0ba0c9eee0
Dan Davison <dandavison7@gmail.com>
2023-08-05 05:57:34 -0400

    cli

--- cli/lib.sh
+++ cli/lib.sh
@@ -1,14 +1,3 @@
 wormhole-env() {
     source /tmp/wormhole.env
 }
-
-wormhole-toggle() {
-    local flag=/tmp/wormhole-land-in-tmux
-    if [ -e $flag ]; then
-        rm $flag
-        echo vscode
-    else
-        touch $flag
-        echo tmux
-    fi
-}

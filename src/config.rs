e7a09e695fdc636981d06016327c9140712eb057
Dan Davison <dandavison7@gmail.com>
2023-08-10 22:09:48 -0400

    Support multiple editors

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,5 @@
+use crate::editor::Editor;
+
+pub const EDITOR: Editor = Editor::VSCode;
 pub const ENV_FILE: &'static str = "/tmp/wormhole.env";
 pub const PROJECTS_FILE: &'static str = "~/.config/wormhole/wormhole-projects.yaml";
--- src/editor.rs
+++ src/editor.rs
@@ -1,28 +1,60 @@
-use std::process::Command;
+use std::{path::Path, process::Command};
 
-use crate::{hammerspoon, project::Project, project_path::ProjectPath, util::warn, WindowAction};
+use crate::{
+    config, hammerspoon, project::Project, project_path::ProjectPath, util::warn, WindowAction,
+};
+
+#[allow(dead_code)]
+pub enum Editor {
+    IntelliJ,
+    VSCode,
+    VSCodeInsiders,
+    PyCharm,
+}
+use Editor::*;
+
+impl Editor {
+    fn open_file_uri(&self, absolute_path: &Path, line: Option<usize>) -> String {
+        let path = absolute_path.to_str().unwrap();
+        let line = line.unwrap_or(1);
+        match self {
+            IntelliJ => format!("idea://open?file={path}&line={line}"),
+            PyCharm => format!("pycharm://open?file={path}&line={line}"),
+            VSCode => format!("vscode://file/{path}:{line}"),
+            VSCodeInsiders => format!("vscode-insiders://file/{path}:{line}"),
+        }
+    }
+
+    pub fn application_name(&self) -> &'static str {
+        match self {
+            IntelliJ => "IntelliJ",
+            PyCharm => "PyCharm",
+            VSCode => "Code",
+            VSCodeInsiders => "Code - Insiders",
+        }
+    }
+}
 
 pub fn open_project(project: &Project, window_action: WindowAction) -> Result<(), String> {
-    hammerspoon::select_vscode_workspace(&project.name, window_action)
+    hammerspoon::select_editor_workspace(config::EDITOR, project, window_action)
 }
 
 pub fn open_path(path: &ProjectPath, window_action: WindowAction) -> Result<(), String> {
     open_project(&path.project, window_action)?;
     if path.relative_path.is_some() {
-        open_vscode_application_at_path(path)?
+        open_editor_application_at_path(path)?
     }
     Ok(())
 }
 
-fn open_vscode_application_at_path(path: &ProjectPath) -> Result<(), String> {
-    let mut uri = format!(
-        "vscode-insiders://file/{}",
-        path.absolute_path().to_str().unwrap()
-    );
-    if let Some((_, Some(line))) = path.relative_path {
-        uri.push_str(&format!(":{}", line));
-    }
-    warn(&format!("vscode::open({uri})"));
+fn open_editor_application_at_path(path: &ProjectPath) -> Result<(), String> {
+    let line = path
+        .relative_path
+        .as_ref()
+        .and_then(|(_, line)| line.to_owned());
+    let uri = config::EDITOR.open_file_uri(&path.absolute_path(), line);
+
+    warn(&format!("open_editor_application_at_path({uri})"));
     if let Ok(_) = Command::new("open").arg(&uri).output() {
         Ok(())
     } else {

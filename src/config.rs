d0c7c2a1a6816c8f1c0e3c810de37aead11c8870
Dan Davison <dandavison7@gmail.com>
2023-08-14 07:07:17 -0400

    Change projects file name

--- src/config.rs
+++ src/config.rs
@@ -4,4 +4,4 @@ use crate::terminal::Terminal;
 pub const EDITOR: Editor = Editor::VSCodeInsiders;
 pub const TERMINAL: Terminal = Terminal::Alacritty { tmux: true };
 pub const ENV_FILE: &'static str = "/tmp/wormhole.env";
-pub const PROJECTS_FILE: &'static str = "~/.config/wormhole/wormhole-projects.yaml";
+pub const PROJECTS_FILE: &'static str = "~/.config/wormhole/wormhole-projects.txt";

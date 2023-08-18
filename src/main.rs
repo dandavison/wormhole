19240bbeb1a55c23eb96f6b74fab443331cc8a3f
Dan Davison <dandavison7@gmail.com>
2023-08-17 22:14:18 -0500

    Refactor to serve https

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,10 @@ use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::thread;
 
-use hyper::server::conn::AddrIncoming;
+// use hyper::server::conn::AddrIncoming;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
-use hyper_rustls::TlsAcceptor;
+// use hyper_rustls::TlsAcceptor;
 use url::form_urlencoded;
 
 use util::warn;
@@ -104,30 +104,45 @@ impl QueryParams {
 #[tokio::main]
 async fn main() {
     project::read_projects();
-    serve_https().await;
+    tokio::join!(serve_http());
 }
 
-async fn serve_https() {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 443));
-    let incoming = AddrIncoming::bind(&addr).unwrap();
+async fn serve_http() {
+    project::read_projects();
+    let addr = SocketAddr::from(([127, 0, 0, 1], 80));
 
-    let certs = load_certs();
-    let key = load_key();
-    let acceptor = TlsAcceptor::builder()
-        .with_single_cert(certs, key)
-        .map_err(|e| error(format!("{}", e)))
-        .with_all_versions_alpn() // wtf?
-        .with_incoming(incoming);
-
-    let make_svc = make_service_fn(|_conn| async {
-        println!("hello");
-        Ok::<_, Infallible>(service_fn(wormhole_spawner))
-    });
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(wormhole_spawner)) });
 
     // Serve forever: a Wormhole service is created for each incoming connection
-    let server = Server::builder(acceptor).serve(make_svc);
+    let server = Server::bind(&addr).serve(make_svc);
 
     if let Err(e) = server.await {
         warn(&format!("server error: {}", e));
     }
 }
+
+// async fn serve_https() {
+//     let addr = SocketAddr::from(([127, 0, 0, 1], 443));
+//     let incoming = AddrIncoming::bind(&addr).unwrap();
+
+//     let certs = load_certs();
+//     let key = load_key();
+//     let acceptor = TlsAcceptor::builder()
+//         .with_single_cert(certs, key)
+//         .map_err(|e| error(format!("{}", e)))
+//         .with_all_versions_alpn() // wtf?
+//         .with_incoming(incoming);
+
+//     let make_svc = make_service_fn(|_conn| async {
+//         println!("hello");
+//         Ok::<_, Infallible>(service_fn(wormhole_spawner))
+//     });
+
+//     // Serve forever: a Wormhole service is created for each incoming connection
+//     let server = Server::builder(acceptor).serve(make_svc);
+
+//     if let Err(e) = server.await {
+//         warn(&format!("server error: {}", e));
+//     }
+// }

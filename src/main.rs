4211e8d522193043271cde9ba05213a97db75207
Dan Davison <dandavison7@gmail.com>
2023-11-03 13:48:38 -0400

    Delete commented HTTPS server

--- src/main.rs
+++ src/main.rs
@@ -39,28 +39,3 @@ async fn serve_http() {
         warn(&format!("server error: {}", e));
     }
 }
-
-// async fn serve_https() {
-//     let addr = SocketAddr::from(([127, 0, 0, 1], 443));
-//     let incoming = AddrIncoming::bind(&addr).unwrap();
-
-//     let certs = load_certs();
-//     let key = load_key();
-//     let acceptor = TlsAcceptor::builder()
-//         .with_single_cert(certs, key)
-//         .map_err(|e| error(format!("{}", e)))
-//         .with_all_versions_alpn() // wtf?
-//         .with_incoming(incoming);
-
-//     let make_service = make_service_fn(|_conn| async {
-//         println!("hello");
-//         Ok::<_, Infallible>(service_fn(wormhole_spawner))
-//     });
-
-//     // Serve forever: a Wormhole service is created for each incoming connection
-//     let server = Server::builder(acceptor).serve(make_service);
-
-//     if let Err(e) = server.await {
-//         warn(&format!("server error: {}", e));
-//     }
-// }

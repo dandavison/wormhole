84e0401ad2916df7145ea4ef20972d254cda4b29
Dan Davison <dandavison7@gmail.com>
2024-06-18 09:35:31 -0400

    CLI utilities

--- cli/lib.sh
+++ cli/lib.sh
@@ -1,3 +1,19 @@
 wormhole-env() {
     source /tmp/wormhole.env
 }
+
+wormhole-cd() {
+    if [ -n "$1" ]; then
+        builtin cd "$1"
+    else
+        builtin cd "$WORMHOLE_PROJECT_DIR"
+    fi
+}
+
+wormhole-switch() {
+    dir="${1:-$PWD}"
+    dir=$(readlink -f "$dir")
+    WORMHOLE_PROJECT_DIR="$dir"
+    WORMHOLE_PROJECT_NAME=$(basename "$dir")
+    cd "$dir"
+}

7750ab6adf469f01100c827c99f992dcfedd5e0c
Dan Davison <dandavison7@gmail.com>
2024-09-02 08:15:51 -0400

    Support opening by path

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::{collections::VecDeque, path::PathBuf};
 
 use hyper::{Body, Response};
 use itertools::Itertools;
@@ -41,11 +41,13 @@ pub fn remove_project(name: &str) -> Response<Body> {
     Response::new(Body::from(format!("removed project: {}", name)))
 }
 
-pub fn open_project(name: &str) -> Response<Body> {
+pub fn open_project(name_or_path: &str) -> Response<Body> {
     // Look up project by name without continuing to hold lock.
     let project = {
         let projects = projects::lock();
-        projects.by_name(name)
+        projects
+            .by_name(name_or_path)
+            .or_else(|| projects.by_path(&PathBuf::from(name_or_path)))
     };
 
     if let Some(p) = project {
@@ -54,11 +56,11 @@ pub fn open_project(name: &str) -> Response<Body> {
             projects::Mutation::Insert,
             Some(crate::wormhole::Application::Terminal),
         );
-        Response::new(Body::from(format!("opened project: {}", name)))
+        Response::new(Body::from(format!("opened project: {}", name_or_path)))
     } else {
         Response::builder()
             .status(hyper::StatusCode::NOT_FOUND)
-            .body(Body::from(format!("project not found: {}", name)))
+            .body(Body::from(format!("project not found: {}", name_or_path)))
             .unwrap()
     }
 }
